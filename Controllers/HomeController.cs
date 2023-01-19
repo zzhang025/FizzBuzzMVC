c252f9449bfc683860875651a7ab5c1311e2e611
zzhang025 <zhangzx45@gmail.com>
2023-01-19 11:21:31 -0800

    MIGRATION

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FizzBuzzMVC.Models;
+
+namespace FizzBuzzMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult FBPage()
+        {
+            FizzBuzz model = new();
+
+            model.FizzValue = 3;
+            model.BuzzValue = 5;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult FBPage(FizzBuzz fizzbuzz)
+        {
+            List<string> fbItems = new();
+
+            bool fizz;
+            bool buzz;
+
+            for (int i = 1; i<=100; i++)
+            {
+                fizz = (i % fizzbuzz.FizzValue == 0);
+                buzz = (i % fizzbuzz.BuzzValue == 0);
+
+                if (fizz == true && buzz == true)
+                {
+                    fbItems.Add("FizzBuzz");
+                } else if (fizz == true)
+                {
+                    fbItems.Add("Fizz");
+                } else if (buzz == true)
+                {
+                    fbItems.Add("Buzz");
+                } else
+                {
+                    fbItems.Add(i.ToString());
+                }
+            }
+
+            fizzbuzz.Result = fbItems;
+
+            return View(fizzbuzz);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/FizzBuzz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FizzBuzzMVC.Models
+{
+    public class FizzBuzz
+    {
+        public int FizzValue { get; set; }
+        public int BuzzValue { get; set; }
+        public List<string> Result { get; set; } = new();
+    } 
+}
